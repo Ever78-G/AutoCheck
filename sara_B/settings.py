be61e1bd06cf73370e5e5ba3499e0fbcf87b5d22
Ever78-G <evegiraldov@gmail.com>
2025-10-06 11:58:23 -0500

    Primer commit

--- sara_B/settings.py
+++ sara_B/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-u^wy6%4api-8+h^9b#%0gykmymcd=-b3bn26q+ia=%vlar+re0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
